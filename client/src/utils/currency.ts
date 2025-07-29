fcb05be89659c2fd7a8e88b26f2bc3a778eea94f
trebatak12 <trebatak12@gmail.com>
2025-07-29 19:06:00 +0000

    fix currency formatting

--- client/src/utils/currency.ts
+++ client/src/utils/currency.ts
@@ -1,8 +1,9 @@
 export function formatCurrency(amount: number): string {
-  return new Intl.NumberFormat('cs-CZ', {
-    style: 'currency',
-    currency: 'CZK',
+  const czk = Math.floor(amount / 100);
+  return new Intl.NumberFormat("cs-CZ", {
+    style: "currency",
+    currency: "CZK",
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
-  }).format(amount);
+  }).format(czk);
 }
